9e0ccbaef2aeb42c9c24f5097ca59b48b62b0816
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-12-04 17:17:40 +0100

    header + notyf

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     sessions: "users/sessions"
   }
 
+  resources :materials, only: [:index, :show]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
