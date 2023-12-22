1164e08306c03e14b19729b37e4a7957ceeaf9ef
Sébastien Gaya <sebastien.gaya@gmail.com>
2023-12-22 14:17:45 +0100

    fix encore

--- config/routes.rb
+++ config/routes.rb
@@ -8,25 +8,23 @@ Rails.application.routes.draw do
 
   draw 'admin'
 
-  localized do
-    scope "(:region_slug)", constraints: lambda { |request|
-      region_slug = request.params[:region_slug]
-      Region.where(slug: region_slug).exists?
-    } do
-      resources :materials, path: 'materiaux', only: [:index, :show] do
-        collection do
-          get ':item_slug/:option_slug' => 'materials#option', as: :option
-        end
-      end
-      resources :projects, path: 'projets', only: [:index, :show] do
-        collection do
-          get ':item_slug/:option_slug' => 'projects#option', as: :option
-        end
+  scope "(:region_slug)", constraints: lambda { |request|
+    region_slug = request.params[:region_slug]
+    Region.where(slug: region_slug).exists?
+  } do
+    resources :materials, path: 'materiaux', only: [:index, :show] do
+      collection do
+        get ':item_slug/:option_slug' => 'materials#option', as: :option
       end
     end
-    get 'regions' => 'regions#index', as: :regions
-    resources :regions, path: "", only: :show
+    resources :projects, path: 'projets', only: [:index, :show] do
+      collection do
+        get ':item_slug/:option_slug' => 'projects#option', as: :option
+      end
+    end
   end
+  get 'regions' => 'regions#index', as: :regions
+  resources :regions, path: "", only: :show
 
   root to: 'home#index'
 end
