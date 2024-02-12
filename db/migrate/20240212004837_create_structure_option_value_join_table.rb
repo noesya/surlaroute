d37f59fa52b5d61acf455509469cd4f1ae2dfae7
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-02-12 03:29:49 +0100

    comment

--- db/migrate/20240212004837_create_structure_option_value_join_table.rb
+++ db/migrate/20240212004837_create_structure_option_value_join_table.rb
@@ -10,16 +10,22 @@ class CreateStructureOptionValueJoinTable < ActiveRecord::Migration[7.1]
     keys = values_count_per_about_and_item.keys
     # [ ["Actor", "f4f55daf-e118-490b-b12b-34d950dec914", "7a69dfa7-1ccd-4946-9e85-fe0b14480cd8"], ... ]
     keys.each do |key|
+      # Better variable names
       about_type, about_id, item_id = key
       values_scope = Structure::Value.where(about_type: about_type, about_id: about_id, item_id: item_id)
+      # Get text from first value
+      value_text = values_scope.first.text
+      # Get all IDs to be destroyed
       value_ids = values_scope.pluck(:id)
+      # Get all IDs of the selected options
       option_ids = values_scope.pluck(:option_id).compact.uniq
-      new_value = Structure::Value.create(about_type: about_type, about_id: about_id, item_id: item_id, text: values_scope.first.text)
+      # Create a new value with all the options inside
+      new_value = Structure::Value.create(about_type: about_type, about_id: about_id, item_id: item_id, text: value_text)
       new_value.option_ids = option_ids
+      # Remove legacy values
       Structure::Value.where(id: value_ids).destroy_all
     end
 
     remove_reference :structure_values, :option, type: :uuid, index: true, foreign_key: { to_table: :structure_options }
-
   end
 end
