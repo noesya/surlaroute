b828501589cd840e75a39933a573c587a8d2e365
Arnaud Levy <contact@arnaudlevy.com>
2024-02-27 15:10:29 +0100

    gem

--- app/models/actor/facets.rb
+++ app/models/actor/facets.rb
@@ -1,13 +1,14 @@
 class Actor::Facets < FacetedSearch::Facets
   def initialize(params)
     super
-
     @model = Actor.published.ordered
-
     filter_with_text :name, {
       title: 'Nom'
     }
+    add_facets_for_items
+  end
 
+  def add_facets_for_items
     items.each do |item|
       add_facet ::FacetedSearch::Facets::Item, item.slug, { item: item }
     end
