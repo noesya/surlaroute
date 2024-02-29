33c7f56fdd032c3e6fd71dceb14f26b5eeaac7d4
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-02-29 10:58:28 +0100

    clean test files

--- test/models/material_test.rb
+++ test/models/material_test.rb
@@ -1,28 +1,3 @@
-# == Schema Information
-#
-# Table name: materials
-#
-#  id              :uuid             not null, primary key
-#  description     :text
-#  name            :string
-#  published       :boolean          default(FALSE)
-#  slug            :string           indexed
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  actor_id        :uuid             indexed
-#  published_by_id :uuid             indexed
-#
-# Indexes
-#
-#  index_materials_on_actor_id         (actor_id)
-#  index_materials_on_published_by_id  (published_by_id)
-#  index_materials_on_slug             (slug)
-#
-# Foreign Keys
-#
-#  fk_rails_d5f504ce55  (published_by_id => users.id)
-#  fk_rails_e6f2c9950b  (actor_id => actors.id)
-#
 require "test_helper"
 
 class MaterialTest < ActiveSupport::TestCase
--- test/models/structure/value/file_test.rb
+++ test/models/structure/value/file_test.rb
@@ -1,22 +1,3 @@
-# == Schema Information
-#
-# Table name: structure_value_files
-#
-#  id         :uuid             not null, primary key
-#  alt        :string
-#  position   :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  value_id   :uuid             not null, indexed
-#
-# Indexes
-#
-#  index_structure_value_files_on_value_id  (value_id)
-#
-# Foreign Keys
-#
-#  fk_rails_162821733e  (value_id => structure_values.id)
-#
 require "test_helper"
 
 class Structure::Value::FileTest < ActiveSupport::TestCase
