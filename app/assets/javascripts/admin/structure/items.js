fd165484b72e281dbc929e3682295b8e903cc905
pabois <pierreandre.boissinot@noesya.coop>
2024-03-04 17:40:42 +0100

    better structure

--- app/assets/javascripts/admin/structure/items.js
+++ app/assets/javascripts/admin/structure/items.js
@@ -2,24 +2,9 @@
 window.ecotheque.structure.items = {
     init: function () {
         'use strict';
-        this.kindField = document.querySelector('#structure_item_kind');
-        this.choices = document.querySelector('#js-choices');
-        this.kindField.addEventListener('change', this.manageChoicesVisibilitiy.bind(this));
-        this.manageChoicesVisibilitiy();
         this.initAddItemCallback();
     },
-
-    manageChoicesVisibilitiy: function () {
-        'use strict';
-        var kind = this.kindField.value,
-            kindsWithChoices = this.kindField.dataset.withChoices.split('|');
-        if (kindsWithChoices.indexOf(kind) !== -1) {
-            this.choices.classList.remove('d-none');
-        } else {
-            this.choices.classList.add('d-none');
-        }
-    },
-
+   
     initAddItemCallback: function () {
         'use strict';
         $('#options').on('cocoon:after-insert', function (e, newElmt) {
