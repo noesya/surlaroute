65891078ba1d4f78ac05fb6c45e25474dafa7b17
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-03-04 16:54:13 +0100

    brezet wheel

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,16 +2,62 @@
 #
 # Table name: projects
 #
-#  id              :uuid             not null, primary key
-#  description     :text
-#  image_alt       :string
-#  image_credit    :string
-#  name            :string
-#  published       :boolean          default(FALSE)
-#  slug            :string           indexed
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  published_by_id :uuid             indexed
+#  id                                         :uuid             not null, primary key
+#  description                                :text
+#  image_alt                                  :string
+#  image_credit                               :string
+#  name                                       :string
+#  published                                  :boolean          default(FALSE)
+#  slug                                       :string           indexed
+#  step0_ecodesign_time                       :boolean          default(FALSE)
+#  step0_ecodesign_time_text                  :text
+#  step0_low_tech                             :boolean          default(FALSE)
+#  step0_low_tech_text                        :text
+#  step0_sustainable_program                  :boolean          default(FALSE)
+#  step0_sustainable_program_text             :text
+#  step1_local_materials                      :boolean          default(FALSE)
+#  step1_local_materials_text                 :text
+#  step1_recycled_materials                   :boolean          default(FALSE)
+#  step1_recycled_materials_text              :text
+#  step1_reusable_materials                   :boolean          default(FALSE)
+#  step1_reusable_materials_text              :text
+#  step2_components_reduced_amount            :boolean          default(FALSE)
+#  step2_components_reduced_amount_text       :text
+#  step2_dismountable                         :boolean          default(FALSE)
+#  step2_dismountable_text                    :text
+#  step2_renewable_energy                     :boolean          default(FALSE)
+#  step2_renewable_energy_text                :text
+#  step3_matter_reduced_amount                :boolean          default(FALSE)
+#  step3_matter_reduced_amount_text           :text
+#  step4_artists_travel_optimization          :boolean          default(FALSE)
+#  step4_artists_travel_optimization_text     :text
+#  step4_separate_packaging                   :boolean          default(FALSE)
+#  step4_separate_packaging_text              :text
+#  step4_transport_sharing                    :boolean          default(FALSE)
+#  step4_transport_sharing_text               :text
+#  step5_energy_efficient                     :boolean          default(FALSE)
+#  step5_energy_efficient_text                :text
+#  step5_low_power_consumption                :boolean          default(FALSE)
+#  step5_low_power_consumption_text           :text
+#  step5_public_area_sorting                  :boolean          default(FALSE)
+#  step5_public_area_sorting_text             :text
+#  step5_public_awareness                     :boolean          default(FALSE)
+#  step5_public_awareness_text                :text
+#  step5_water_efficient                      :boolean          default(FALSE)
+#  step5_water_efficient_text                 :text
+#  step5_zero_waste                           :boolean          default(FALSE)
+#  step5_zero_waste_text                      :text
+#  step6_extended_lifetime                    :boolean          default(FALSE)
+#  step6_extended_lifetime_text               :text
+#  step7_closed_loop_recycling                :boolean          default(FALSE)
+#  step7_closed_loop_recycling_text           :text
+#  step7_composted_materials                  :boolean          default(FALSE)
+#  step7_composted_materials_text             :text
+#  step7_disassembly_five_stream_sorting      :boolean          default(FALSE)
+#  step7_disassembly_five_stream_sorting_text :text
+#  created_at                                 :datetime         not null
+#  updated_at                                 :datetime         not null
+#  published_by_id                            :uuid             indexed
 #
 # Indexes
 #
@@ -28,6 +74,7 @@ class Project < ApplicationRecord
   include Regional
   include Slugged
   include Structured
+  include WithBrezetWheel
 
   has_and_belongs_to_many :actors
   has_and_belongs_to_many :materials
