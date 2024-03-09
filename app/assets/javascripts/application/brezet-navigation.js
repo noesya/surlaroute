b99ec76164cb76c5b9fb2056c2a33f738c005129
Olivia206 <olivia.simonet206@gmail.com>
2024-03-09 18:54:48 +0100

    fixes

---
+++ app/assets/javascripts/application/brezet-navigation.js
@@ -0,0 +1,63 @@
+window.ecosystem = window.ecosystem || {};
+
+window.ecosystem.brezetNavigation = {
+    init: function () {
+        'use strict';
+        this.dom = document.querySelector('.brezet-wheel-container');
+        
+        if (!this.dom) {
+            return;
+        }
+
+        this.navigation = this.dom.querySelector('#brezet-navigation');
+        this.activeSections = this.dom.querySelectorAll('h3[id^="step-"]');
+
+        this.initIntersectionObserver();
+        this.disableLinks();
+    },
+
+    disableLinks: function () {
+        const sectionIds = Array.from(this.activeSections).map(section => section.getAttribute('id'));
+
+        this.navigation.querySelectorAll('a').forEach(link => {
+            const href = link.getAttribute('href').substring(1);
+
+            if (!sectionIds.includes(href)) {
+                link.classList.add('is-disabled');
+            }
+        });
+
+    },
+
+    initIntersectionObserver: function () {
+        this.observedEl = this.dom;
+
+        if (!this.observedEl) {
+            return;
+        }
+        this.observeSections();
+    },
+
+    observeSections: function () {
+        function handleIntersection(entries, observer) {
+            entries.forEach(entry => {
+                this.id = entry.target.getAttribute('id');
+                this.link = document.querySelector(`#brezet-navigation a[href="#${this.id}"]`);
+
+                if (entry.isIntersecting) {
+                    this.link.classList.add('active');
+                } else {
+                    this.link.classList.remove('active');
+                }
+            });
+        }
+
+        this.observer = new IntersectionObserver(handleIntersection, { threshold: [0, 0.25, 0.75, 1], rootMargin: '0px 0px -50% 0px'  });
+
+        this.activeSections.forEach(section => {
+            this.observer.observe(section);
+        });
+    }
+};
+
+window.ecosystem.brezetNavigation.init();
