de02316ade8debafc824525cabb0a81405c78367
pabois <pierreandre.boissinot@noesya.coop>
2024-03-11 15:12:56 +0100

    body classes

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,16 +1,5 @@
 module Admin::ApplicationHelper
 
-  def controller_class
-    "#{controller_name.gsub('/', '--')}"
-  end
-
-  def body_classes
-    classes = "controller-#{controller_class}"
-    classes += " action-#{action_name}"
-    classes += " #{controller_class}-#{action_name}"
-    classes
-  end
-
   def button_classes(additional = '', **options)
     classes = "btn btn-dark btn-sm #{additional}"
     classes += ' disabled' if options[:disabled]
