95a55a9a48c023e9a94adbb164289f523298f7d7
alexisben <alex@noesya.coop>
2024-03-12 06:16:45 +0100

    polish footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     breadcrumb
-    @toolbox_page = Page.find_by(path: "boite-a-outils")
+    @toolbox_page = Page.find_by(internal_identifier: 'boite-a-outils')
   end
 end
