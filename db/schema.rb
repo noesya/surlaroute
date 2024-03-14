4edd9c288f5837a8accd9bd96372f86afdc5f15d
Olivia206 <olivia.simonet@mmibordeaux.com>
2024-03-14 09:54:18 +0100

    offers btn

--- db/schema.rb
+++ db/schema.rb
@@ -223,8 +223,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_161944) do
     t.uuid "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "slug"
     t.string "body_class", default: ""
+    t.string "slug"
     t.index ["parent_id"], name: "index_pages_on_parent_id"
   end
 
