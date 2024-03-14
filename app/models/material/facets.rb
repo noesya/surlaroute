21b5c74673001d86d8f6292fade18d0c6b3c97f1
pabois <pierreandre.boissinot@noesya.coop>
2024-03-14 14:04:01 +0100

    close #84

--- app/models/material/facets.rb
+++ app/models/material/facets.rb
@@ -4,7 +4,8 @@ class Material::Facets < FacetedSearch::FacetsWithItems
     @model = Material.published.ordered
     @class_name = 'Material'
     filter_with_text :name, {
-      title: 'Nom'
+      title: Material.human_attribute_name('name'),
+      placeholder: I18n.t('search.search_in_name')
     }
     add_facets_for_items
   end
