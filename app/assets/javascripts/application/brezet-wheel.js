f7194d51e928c4a091cc0bf3a68eaed47c048deb
Olivia206 <olivia.simonet@mmibordeaux.com>
2024-03-14 13:09:30 +0100

    commentaries

--- app/assets/javascripts/application/brezet-wheel.js
+++ app/assets/javascripts/application/brezet-wheel.js
@@ -25,10 +25,27 @@ window.ecosystem.brezetWheel = {
     },
     scrollAnimation: function () {
         this.scrollListener = () => {
+            // on récupère la hauteur de container Top dans la page par rapport au scroll : 
+            // quand brezetContainer n'est pas visible : valeur positive
+            // quand c'est visible : 0 ou valeur positive
             var containerTop = this.brezetContainer.getBoundingClientRect().top;
+
+            // on récupère la distance entre le haut brezet-details et le haut de son parent (brezet-wheel-container)
+            // on divise par 2 pour ajuster la valeur désirée
             var distance = this.content.offsetTop / 2;
+
+            // on détermine un ratio compris entre 0 et 1 -> il va de 1 à 0 donc il faut l'inverser avce 1 - ...
+            // même si notre ratio est écouté pdt tout le scroll, il ne prendra ses bonnes valeurs qu'une fois entre 1 et 0
+            // on divise containerTop par distance pour avoir le temps de scroll désiré pour l'animation
+            // on ajoute un - car on est à l'inverse
             var ratio =1 - Math.max(0, Math.min(1,  -containerTop / distance));
+
+            // pour que notre élément reste centré, on lui ajoute la valeur des gouttières bootstrap
+            // on multiplie le tout par le ratio pour obtenir une valeur décroissante entre 50% et 0%
             this.figure.style.transform = `translateX(calc( (50% + var(--bs-gutter-x)) * ${ratio}))`;
+
+            // pour chaque texte, on utilise le ratio pour faire diminuer l'opacité des textes
+            // pas besoin de plus de valeur car on va de 1 à 0
             this.texts.forEach((text) => {
                 text.style.opacity = ratio;
             });
@@ -43,12 +60,6 @@ window.ecosystem.brezetWheel = {
         // 3) on passe la figure en opacity 1
         this.scrollListener();
         this.figure.style.opacity = 1;
-    },
-    removeScrollListener: function () {
-        if (this.scrollListener) {
-            window.removeEventListener("scroll", this.scrollListener);
-            this.scrollListener = null;
-        }
     }
 };
 
