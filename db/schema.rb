6e071be103cf5382e7dcb0b8c1c1d0755a481cba
pabois <pierreandre.boissinot@noesya.coop>
2024-03-18 10:40:39 +0100

    yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_15_140328) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_18_092913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -94,6 +94,21 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_15_140328) do
     t.uuid "actor_id", null: false
     t.uuid "technic_id", null: false
     t.index ["technic_id", "actor_id"], name: "index_actors_technics_on_technic_id_and_actor_id"
+  end
+
+  create_table "definition_alisases", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "definition_id", null: false
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["definition_id"], name: "index_definition_alisases_on_definition_id"
+  end
+
+  create_table "definitions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "good_job_batches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -435,6 +450,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_15_140328) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "actors", "users", column: "published_by_id"
+  add_foreign_key "definition_alisases", "definitions"
   add_foreign_key "materials", "actors"
   add_foreign_key "materials", "users", column: "published_by_id"
   add_foreign_key "page_blocks", "pages"
