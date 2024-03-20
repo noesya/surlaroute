ef9716a6bc4ad75542cc9db883e23aac961d329b
Arnaud Levy <contact@arnaudlevy.com>
2024-03-20 18:06:46 +0100

    Clean

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,18 +3,8 @@ class MembersController < ApplicationController
   include ApplicationHelper
 
   def index
-    @members = User.lab_member.ordered.page(params[:page])
+    @members = Actor.lab_member.ordered.page(params[:page])
     breadcrumb
-  end
-
-  def show
-    @member = User.lab_member.find_by!(id: params[:id])
-    @actors = @member.actors.ordered.page(params[:actors_page]).per(8)
-    @projects = @member.projects.ordered.page(params[:projects_page]).per(8)
-    @technics = @member.technics.ordered.page(params[:technics_page]).per(8)
-    @materials = @member.materials.ordered.page(params[:materials_page]).per(8)
-    breadcrumb
-    add_breadcrumb @member
   end
 
   protected
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  include ApplicationHelper
+
+  def index
+    @users = User.ordered.page(params[:page])
+    breadcrumb
+  end
+
+  def show
+    @user = User.find_by!(id: params[:id])
+    @actors = @user.actors.ordered.page(params[:actors_page]).per(8)
+    @projects = @user.projects.ordered.page(params[:projects_page]).per(8)
+    @technics = @user.technics.ordered.page(params[:technics_page]).per(8)
+    @materials = @user.materials.ordered.page(params[:materials_page]).per(8)
+    breadcrumb
+    add_breadcrumb @user
+  end
+
+  protected
+
+  def breadcrumb
+    super
+    add_breadcrumb t('ui.users.title'), users_path
+  end
+end
