4e7ae9df0a8103988c575325023deecd9df12de0
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-03-25 01:31:21 +0100

    permission

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_164617) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_24_235248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -408,6 +408,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_22_164617) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "paid_amount"
+    t.date "expiration_date"
     t.index ["product_id"], name: "index_subscriptions_on_product_id"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
