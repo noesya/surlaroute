2e9af502180862d6324408e763bca7d8ee477ef7
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-03-25 01:28:49 +0100

    condition job

--- app/jobs/subscription/expiration_check_job.rb
+++ app/jobs/subscription/expiration_check_job.rb
@@ -1,6 +1,9 @@
 class Subscription::ExpirationCheckJob < ApplicationJob
   def perform
-    Subscription.includes(:user).where(expiration_date: Date.today).find_each do |subscription|
+    Subscription
+      .joins(:user)
+      .where(users: { role: :subscriber })
+      .where(expiration_date: Date.today).find_each do |subscription|
       user = subscription.user
       next if user.active_subscription.present?
       user.update(role: :visitor)
