104b126b0c6261f43700613502250464ed0bc044
alexisben <alex@noesya.coop>
2024-03-27 16:24:53 +0100

    fix gallery

--- app/assets/javascripts/application/splide.js
+++ app/assets/javascripts/application/splide.js
@@ -7,7 +7,7 @@ if (sliders) {
     for (i = 0; i < sliders.length; i += 1) {
         new Splide(sliders[i], {
             pagination: false,
-            perPage: 2,
+            // perPage: 2,
             autoHeight: true,
             autoWidth: true,
             breakpoints: {
