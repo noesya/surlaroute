7bc0bea15950c76a0f20507add61cad021f40b80
pabois <pierreandre.boissinot@noesya.coop>
2024-03-28 11:11:33 +0100

    remove comment

--- app/services/faceted_search/facets/item.rb
+++ app/services/faceted_search/facets/item.rb
@@ -24,7 +24,6 @@ module FacetedSearch
 
     def add_scope(scope)
       return scope if params_array.blank?
-      # TODO limiter à ceux qui résultats
       alias_suffix = "_#{item.id.gsub('-', '_')}"
       model = facets.class_name.safe_constantize
       options_table_alias = model.connection.quote_column_name("structure_options#{alias_suffix}")
