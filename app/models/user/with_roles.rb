4d4d81c13e85480a9810213c71edf6c462850767
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-03-28 12:20:35 +0100

    fix

--- app/models/user/with_roles.rb
+++ app/models/user/with_roles.rb
@@ -12,7 +12,7 @@ module User::WithRoles
     before_validation :check_modifier_role
 
     scope :for_role, -> (role) { where(role: role) }
-    # "Or" condition in incoming scope is because someone might have been set as author when subscriber, and is now only a visitor. 
+    # "Or" condition in incoming scope is because someone might have been set as author when subscriber, and is now only a visitor.
     # We don't want to lose the actual author even if he's not subscriber anymore
     scope :possible_authors, -> (author_ids) { not_visitor.or(where(id: author_ids)) }
 
@@ -29,7 +29,7 @@ module User::WithRoles
 
     def possible_author?
       # mirror of the "possible_authors" scope
-      !visitor
+      !visitor?
     end
 
     def has_access_to_website_and_description?
