6b67d95893d124f87bf419bcb15cdad71b1f8f31
pabois <pierreandre.boissinot@noesya.coop>
2024-03-28 12:04:09 +0100

    add published filer

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -8,13 +8,13 @@ class RegionsController < ApplicationController
 
   def show
     @region = Region.find_by!(slug: params[:id])
-    @all_materials = @region.materials.published
+    @all_materials = @region.materials.published.ordered
     @materials = @all_materials.limit(4)
-    @all_projects = @region.projects.published
+    @all_projects = @region.projects.published.ordered
     @projects = @all_projects.limit(4)
-    @all_actors = @region.actors.published
+    @all_actors = @region.actors.published.ordered
     @actors = @all_actors.limit(4)
-    @all_technics = @region.technics.published
+    @all_technics = @region.technics.published.ordered
     @technics = @all_technics.limit(4)
     breadcrumb
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by!(id: params[:id])
-    @actors = @user.actors.ordered.page(params[:actors_page]).per(8)
-    @projects = @user.projects.ordered.page(params[:projects_page]).per(8)
-    @technics = @user.technics.ordered.page(params[:technics_page]).per(8)
-    @materials = @user.materials.ordered.page(params[:materials_page]).per(8)
+    @actors = @user.actors.published.ordered.page(params[:actors_page]).per(8)
+    @projects = @user.projects.published.ordered.page(params[:projects_page]).per(8)
+    @technics = @user.technics.published.ordered.page(params[:technics_page]).per(8)
+    @materials = @user.materials.published.ordered.page(params[:materials_page]).per(8)
     breadcrumb
     add_breadcrumb @user
   end
