c5b1af6cfe79500ef28bf6d357908e474b6d7839
alexisben <alex@noesya.coop>
2024-04-08 16:13:41 +0200

    sticky

--- app/assets/javascripts/application/menu.js
+++ app/assets/javascripts/application/menu.js
@@ -2,11 +2,33 @@ window.ecotheque = window.ecotheque || {};
 
 window.ecotheque.menu = {
     previousScrollY: 0,
+    isOpened: false,
     init: function () {
         'use strict';
         this.dom = document.querySelector('#document-header');
+        this.navbar = this.dom.querySelector('.navbar-collapse');
+
+        this.navbar.addEventListener('show.bs.collapse', function () {
+            this.dom.classList.add('is-opened');
+        }.bind(this));
+
+        this.navbar.addEventListener('hide.bs.collapse', function () {
+            this.dom.classList.remove('is-opened');
+        }.bind(this));
+
         window.addEventListener('scroll', this.onScroll.bind(this));
         window.addEventListener('touchmove', this.onScroll.bind(this));
+
+        this.onScroll();
+    },
+    onClickButton: function () {
+        'use strict';
+        this.isOpened = this.navbar.classList.contains('show');
+        if (this.isOpened) {
+            this.dom.classList.add('is-opened');
+        } else {
+            this.dom.classList.remove('is-opened');
+        }
     },
     onScroll: function () {
         'use strict';
