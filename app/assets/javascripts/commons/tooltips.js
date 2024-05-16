97cda8b246fa351fcb3c0e6753fd40c67e7f8fc0
pabois <pierreandre.boissinot@noesya.coop>
2024-05-16 17:16:22 +0200

    tooltips html

--- app/assets/javascripts/commons/tooltips.js
+++ app/assets/javascripts/commons/tooltips.js
@@ -3,6 +3,6 @@ window.addEventListener('DOMContentLoaded', function () {
     'use strict';
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
+        return new bootstrap.Tooltip(tooltipTriggerEl, { html: true })
     });
 })
