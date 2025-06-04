fa0535bcb863e04f4d622ba6e2e7fbe302b01331
Arnaud Levy <contact@arnaudlevy.com>
2025-06-04 12:19:59 +0200

    breadcrumb régions

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -3,7 +3,6 @@ class RegionsController < ApplicationController
   def index
     @regions = Region.all.ordered.page(params[:page])
     breadcrumb
-    add_breadcrumb Region.model_name.human(count: 2), regions_path(region_slug: nil)
   end
 
   def show
