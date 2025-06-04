3496e6d44e53a839c5ff691b4eb4f118b5c36b61
Arnaud Levy <contact@arnaudlevy.com>
2025-06-04 18:28:34 +0200

    pdf done

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,23 +2,32 @@ class DownloadsController < ApplicationController
   before_action :authenticate_user!
 
   def download_from_page_block
-
+    @blob = ActiveStorage::Blob.find(params[:blob])
+    block = Page::Block.find(params[:block])
+    @about = block.page
+    log_and_send
   end
 
   def download_from_structure_item
     file = Structure::Value::File.find(params[:file])
-    about = file.value.about
+    @about = file.value.about
     attachment = file.file
-    blob = attachment.blob
-    data = URI.parse(attachment.url).read
+    @blob = attachment.blob
+    log_and_send
+  end
+
+  protected
+
+  def log_and_send
+    data = URI.parse(@blob.url).read
     User::Log.create(
       user: current_user,
-      about: about,
-      blob: blob
+      about: @about,
+      blob: @blob
     )
     send_data data,
-              type: "#{attachment.content_type}",
-              disposition: "attachment; filename=#{attachment.filename.to_s}"
+              type: "#{@blob.content_type}",
+              disposition: "attachment; filename=#{@blob.filename.to_s}"
   end
 
 end
