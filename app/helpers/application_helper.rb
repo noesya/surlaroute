d7be0b5f645f458abc2c06be939afce8d8e1b564
Olivia206 <olivia.simonet@mmibordeaux.com>
2025-06-05 17:50:32 +0200

    colored menu in dropdowns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -71,6 +71,10 @@ module ApplicationHelper
     end
   end
 
+  def get_toolbox_page
+    toolbox_page = Page.find_by(internal_identifier: 'boite-a-outils')
+  end
+
   private
 
   def definitions_mapping
