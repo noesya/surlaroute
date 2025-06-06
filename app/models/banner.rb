4a54c3f6292a3e511bcfc042638c5e24b45b01c1
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-06-06 16:11:41 +0200

    banner default published + options in model

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -4,12 +4,17 @@
 #
 #  id               :uuid             not null, primary key
 #  background_color :string
-#  published        :boolean
+#  published        :boolean          default(FALSE)
 #  text             :text
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #
 class Banner < ApplicationRecord
-
-
+  BACKGROUND_COLOR_OPTIONS = {
+    'Vert' => '#beffc1',
+    'Violet écosystème' => '#f4d4ff',
+    'Jaune projet' => '#f5ff85',
+    'Bleu matériaux' => '#b7e0ff',
+    'Orange technique' => '#ff8355'
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_06_131305) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_06_140445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -106,7 +106,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_06_131305) do
   create_table "banners", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.text "text"
     t.string "background_color"
-    t.boolean "published"
+    t.boolean "published", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
