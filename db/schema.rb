e261d26e214c05fc428cf4390e0c4fce02be583e
Olivia206 <olivia.simonet@mmibordeaux.com>
2025-06-06 09:52:55 +0200

    login page: texts and columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_06_071356) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_05_145326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
