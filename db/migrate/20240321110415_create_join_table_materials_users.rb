0198db7be7b5d40c1805afbe68d5eaed1b47741c
Arnaud Levy <contact@arnaudlevy.com>
2025-06-10 10:47:40 +0200

    Fix #7

--- db/migrate/20240321110415_create_join_table_materials_users.rb
+++ db/migrate/20240321110415_create_join_table_materials_users.rb
@@ -4,9 +4,9 @@ class CreateJoinTableMaterialsUsers < ActiveRecord::Migration[7.1]
       t.index [:material_id, :user_id]
     end
 
-    Material.where.not(published_by_id: nil).each do |material|
-      material.authors << User.find(material.published_by_id)
-    end
+    # Material.where.not(published_by_id: nil).each do |material|
+    #   material.authors << User.find(material.published_by_id)
+    # end
 
   end
 end
