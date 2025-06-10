29a44f45bffb8cd575f15d12856066f564129375
Arnaud Levy <contact@arnaudlevy.com>
2025-06-10 10:33:14 +0200

    fix

--- db/migrate/20240311144617_feed_page_slugs.rb
+++ db/migrate/20240311144617_feed_page_slugs.rb
@@ -1,8 +1,8 @@
 class FeedPageSlugs < ActiveRecord::Migration[7.1]
   def change
-    Page.find_each do |page|
-      page.slug = page.path
-      page.save
-    end
+    # Page.find_each do |page|
+    #   page.slug = page.path
+    #   page.save
+    # end
   end
 end
