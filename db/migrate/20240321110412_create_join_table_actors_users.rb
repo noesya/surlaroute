6bcf8c8681a29853cbce39c9c2cd7701c756de67
Arnaud Levy <contact@arnaudlevy.com>
2025-06-10 10:37:28 +0200

    fix #5

--- db/migrate/20240321110412_create_join_table_actors_users.rb
+++ db/migrate/20240321110412_create_join_table_actors_users.rb
@@ -4,9 +4,9 @@ class CreateJoinTableActorsUsers < ActiveRecord::Migration[7.1]
       t.index [:actor_id, :user_id]
     end
 
-    Actor.where.not(published_by_id: nil).each do |actor|
-      actor.authors << User.find(actor.published_by_id)
-    end
+    # Actor.where.not(published_by_id: nil).each do |actor|
+    #   actor.authors << User.find(actor.published_by_id)
+    # end
 
   end
 end
