74d75a17721e935619897015d03db50929bafafe
Arnaud Levy <contact@arnaudlevy.com>
2025-06-12 14:49:58 +0200

    years

--- app/models/tour/facets.rb
+++ app/models/tour/facets.rb
@@ -9,6 +9,9 @@ class Tour::Facets < FacetedSearch::FacetsWithItems
       title: Tour.human_attribute_name('name'),
       placeholder: I18n.t('search.search_in_name')
     }
+    add_facet FacetedSearch::Facets::Year, :year, {
+      title: Tour.human_attribute_name('year')
+    }
     add_facets_for_items
     add_facets_for_regions unless @region.present?
   end
