51037bfa917b21b4a158d6bcc5755ef37dfb06d7
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-08-11 16:35:08 +0200

    fix sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,9 +3,7 @@ class SitemapController < ApplicationController
     @home_page = Page.find_by(internal_identifier: 'home')
     @pages = Page.where.not(id: @home_page.id).select(:id, :path, :updated_at)
     @actors = Actor.published.select(:id, :slug, :updated_at)
-    @materials = Material.published.select(:id, :slug, :updated_at)
-    @projects = Project.published.select(:id, :slug, :updated_at)
-    @technics = Technic.published.select(:id, :slug, :updated_at)
+    @tours = Tour.published.select(:id, :slug, :updated_at)
     @regions = Region.all
 
     render layout: false
